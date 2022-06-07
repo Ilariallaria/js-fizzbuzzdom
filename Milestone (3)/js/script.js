4f94d317e2d9063a90b9960b24931a34c7fa1dfc
Ilariallaria <hilary.lion@gmail.com>
2022-06-07 18:21:25 +0200

    milestone3

---
+++ Milestone (3)/js/script.js
@@ -0,0 +1,26 @@
+// MILESTONE 3
+// Per ogni numero, dato un container nel DOM,
+// appendi un elemento html con il numero
+// o la stringa corretta.
+// Applica uno stile differente a seconda del valore
+// dell'indice per i multipli di 3
+// per i multipli di 5
+// per i valori che sono sia multipli di 3 che di 5
+// (seguendo le regole della Milestone 1).
+
+const container = document.querySelector('.container');
+
+for( let i =1; i<=100; i++){
+    let numberType;
+    if(i %3 ===0 && i % 5===0){
+        numberType = 'buzzfizz';
+    }
+    else if(i % 3 ===0){
+        numberType = 'fizz';
+    }
+    else if(i % 5===0){
+        numberType = 'buzz'
+    }
+    const colorBox = `<div class="box ${numberType}">${i}</div>`;
+    container.innerHTML+= colorBox;
+}
